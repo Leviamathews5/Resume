2a821e0c4b34cbbdb6cc9c194dca0a9f8f103a12
Mathews <lmathews1@madisoncollege.edu>
2023-12-04 13:26:25 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,20 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <head>
+
+      </head>
+      <body>
+        <header>
+
+        </header>
+        <h1>
+          Levi Mathews
+        </h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Created 12/4/2023
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </body>
     </div>
   );
 }
